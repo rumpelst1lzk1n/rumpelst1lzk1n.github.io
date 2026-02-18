f2cb83e0b3f369d445bc3bdef96dc37252f5959f
rumpelst1lzk1n <93686327+rumpelst1lzk1n@users.noreply.github.com>
2026-02-18 17:13:45 +0000

    Deploying to gh-pages from @ rumpelst1lzk1n/rumpelst1lzk1n.github.io@b3186e3ef3b6fa933bf96aab102b413579e0a60a 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0ffad392.js',
+                path_to_root + 'searchindex-ac604489.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
